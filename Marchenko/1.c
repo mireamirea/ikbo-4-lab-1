bb13390433083566e1ccf70ef1eb9f2bfb0d8077
MarMaxim <maksim.marchenko.1997@mail.ru>
2017-06-05 15:18:49 +0300

    Add files via upload

--- Marchenko/1.c
+++ Marchenko/1.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <locale.h>
+
 
 typedef struct Packet
 {
@@ -11,23 +11,23 @@ float fnum;
 
 int main(int argc, char *argv[])
 {
-    setlocale(LC_ALL,"RUS");
+
     char rz[255];
     FILE *f;
     Pack temp;
     if (argc>1)
-        { if ((f = fopen(argv[1],"rb"))==0) { printf("Не могу открыть этот фаил\n"); return 1; } }
-    else {printf("Введите имя фаила\n");
+        { if ((f = fopen(argv[1],"rb"))==0) { printf("Open error\n"); return 1; } }
+    else {printf("Enter file name:\n");
         scanf("%s",rz);
         if (!(f=fopen(rz,"rb"))) {
-            printf("Не могу открыть этот фаил");
+            printf("Open error");
             return 0;
             }
         }
     while ((fread(&temp,sizeof(Pack),1,f)!=0))
         printf("%d\t%s\t%.2f\n",temp.num,temp.str,temp.fnum);
     fseek(f, 0, SEEK_END);
-	printf("Размер файла %d%s", ftell(f), " байт\n");
+	printf("File size: %d%s", ftell(f), " byte\n");
 	fclose(f);
     return 0;
 }
